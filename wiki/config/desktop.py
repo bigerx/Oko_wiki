c36e20e377ef98fae574ed57124259c102f6fe25
bigerx <127696805+bigerx@users.noreply.github.com>
2023-04-18 02:59:32 +0200

    Update desktop.py

--- wiki/config/desktop.py
+++ wiki/config/desktop.py
@@ -4,10 +4,10 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Wiki",
+			"module_name": "Oko_Wiki",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Wiki"),
+			"label": _("OkoWiki"),
 		}
 	]
